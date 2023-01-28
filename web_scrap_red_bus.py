764a0603b5d8b196ac9938837e47479f103bc2c3
Soumyadeepaul <92711266+Soumyadeepaul@users.noreply.github.com>
2023-01-28 15:24:24 +0530

    Update web_scrap_red_bus.py

--- web_scrap_red_bus.py
+++ web_scrap_red_bus.py
@@ -5,8 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 import pandas as pd
 def web(place1,place2,date):
-    driver = webdriver.Chrome(executable_path='E:\anaconda\chromedriver.exe')
-    driver.maximize_window()
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path="E:\anaconda\chromedriver.exe",options=options)
     driver.get('https://www.redbus.in/')
     driver.implicitly_wait(5)
     driver.find_element(By.XPATH, '//*[@id="search"]/div/div[1]')
